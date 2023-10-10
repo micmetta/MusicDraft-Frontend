9daa6211b4824842d84e4a5437244afd80e4500a
Pietro Sangermano <pietro.sangermano97@gmail.com>
2023-10-10 11:04:19 +0200

    Commit Con merketplace e carte e homepage

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM node:14
 WORKDIR /app
 COPY . .
 RUN npm install
-RUN ng build --prod
-RUN npm start
+COPY ./dist /app/dist
+EXPOSE 4200
+CMD ["npm", "start"]
--- update-proxy-config.js
+++ update-proxy-config.js
@@ -2,19 +2,25 @@ const { exec } = require('child_process');
 const fs = require('fs');
 
 const child = require('child_process').exec('minikube service api-gateway --url');
-child.stdout.on('data',(data)=>{
+child.stdout.on('data', (data) => {
+  const apiUrl = data.trim(); // Rimuovi eventuali spazi bianchi
+
+  // Assicurati che l'URL includa il protocollo (http o https)
+  const completeUrl = apiUrl.startsWith('http') ? apiUrl : `http://${apiUrl}`;
+
   const proxyConfig = {
-    '/api/v1/': {
-      target: data.slice(0,-1),
-      secure: false
+    '/api/v1': {
+      target: completeUrl, // Utilizza l'URL completo come destinazione
+      secure: false,
     }
   };
 
   // Scrivi il file proxy.conf.json con l'URL corrente
   fs.writeFileSync('proxy.conf.json', JSON.stringify(proxyConfig, null, 2));
 
-  console.log(`Configurazione proxy aggiornata con successo con l'URL: ${data}`);
+  console.log(`Configurazione proxy aggiornata con successo con l'URL: ${completeUrl}`);
 
+  // Avvia il server Angular con il proxy configurato
   const ngServeChild = require('child_process').exec('ng serve --proxy-config proxy.conf.json');
 
   ngServeChild.stdout.on('data', (ngServeData) => {
@@ -25,9 +31,3 @@ child.stdout.on('data',(data)=>{
     console.error(`Errore di 'ng serve': ${ngServeError}`);
   });
 });
-
-
-
-
-
-
